079d138ac0d97c9675e63d0374b0ae6631c794ae
Gaurang Pancholi <gaurang1942@gmail.com>
2024-01-06 02:44:15 +0530

    first commit

---
+++ src/test/java/com/novlence/parking/parkingbackend/ParkingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novlence.parking.parkingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
